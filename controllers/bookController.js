568b739d19aef90ffeeb4d54a3d97b3220cb3cae
yobars <hellobars19@gmail.com>
2022-10-23 10:10:58 +0600

    Lab 4 added

---
+++ controllers/bookController.js
@@ -0,0 +1,43 @@
+const books = require("./../models/books");
+const bookModel = require("./../models/books");
+
+const getBookList = async (req, res) => {
+  let data = [];
+  let books = [];
+  try {
+    data = await bookModel.find();
+    console.log(data);
+    data.forEach((book) => {
+      books.push({ name: book.name, author: book.author, genre: book.genre });
+    });
+  } catch (error) {
+    console.log(error);
+  } finally {
+    res.render("bookList", { books: books });
+  }
+};
+
+const getBook = (req, res) => {
+  res.render("addBooks");
+};
+
+const postBook = (req, res) => {
+  const data = new bookModel({
+    name: req.body.name,
+    author: req.body.author,
+    genre: req.body.genre,
+  });
+  data
+    .save()
+    .then(() => {
+      console.log("Data Saved Successfully!");
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .finally(() => {
+      res.redirect("/books");
+    });
+};
+
+module.exports = { getBookList, getBook, postBook };
---
+++ routes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const homeController = require("./controllers/homeController");
+const bookController = require("./controllers/bookController");
+
+router.get("/", homeController.getHome);
+router.get("/book-list", bookController.getBookList);
+router.get("/books", bookController.getBook);
+router.post("/books", bookController.postBook);
+
+module.exports = router;
